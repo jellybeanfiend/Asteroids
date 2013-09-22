922b6269dde36b486a0eaddbdcb64536f463423e
Sevske <tookieblade@gmail.com>
2013-09-22 03:39:05 -0400

    Testing ssh access

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ $(document).ready(function(){
 		forward: false,
 		fire: false,
 	}
+	//yay!
 
 	asteroid_shapes = [
 		[[-3,1],[-3,-1],[1,-3],[3,-1],[3,2],[1,3],[0,1],[0,3],[-3,2],[-1,1],[-3,1]],
